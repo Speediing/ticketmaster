255c02f29055a7827dce4d5c753d7e19ec89dc72
Jason Wiker <Jason.wiker@vercel.com>
2023-03-22 17:16:37 -0600

    .

--- apps/web/pages/api/revalidate.ts
+++ apps/web/pages/api/revalidate.ts
@@ -1,12 +1,11 @@
 export default async function handler(req: any, res: any) {
-  // Check for secret to confirm this is a valid request
-
   try {
     // This should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     await res.revalidate("/");
     return res.json({ revalidated: true });
   } catch (err) {
+    console.log(err);
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
     return res.status(500).send("Error revalidating");
