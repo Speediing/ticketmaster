e06541f2df90eddb89c77881f67c7e6f233c3ae3
Jason Wiker <Jason.wiker@vercel.com>
2023-04-25 10:10:22 -0600

    Update middleware.ts

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -4,5 +4,6 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  console.log("a");
   return NextResponse.redirect(new URL("https://google.com", request.url));
 }
