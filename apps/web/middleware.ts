cc4d3e269e17e0ac6b86602385a254a8c05833cf
Jason Wiker <jason.wiker@gmail.com>
2023-05-10 14:25:48 -0600

    Update middleware.ts

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -4,5 +4,5 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("https://google.com", request.url));
+  return NextResponse();
 }
