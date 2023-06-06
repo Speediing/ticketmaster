fa16eca08109121231982f9c37ee43f8db6cc16e
Jason Wiker <Jason.wiker@vercel.com>
2023-06-06 09:46:18 -0600

    Update head.tsx

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -6,20 +6,11 @@ export default function Head() {
         Ticketmaster Site
       </title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta
-        property="og:title"
-        content="Tickets for concerts, theatre, football, family days out. Official Ticketmaster Site"
-      />
-      <meta
-        property="og:description"
-        content="Ticketmaster: All your entertainment needs under one virtual roof with tickets for theatre, concerts, sport, family events, clubs and more."
-      />
+      <meta property="og:title" content="Lowes" />
+      <meta property="og:description" content="Lowes" />
       <meta property="og:image" content="/api/og" />
 
-      <link
-        rel="shortcut icon"
-        href="https://uk.tmconst.com/production-9-409-0-5206500/images/favicon.ico"
-      />
+      <link rel="shortcut icon" href="https://www.lowes.com/favicon.ico" />
     </>
   );
 }
